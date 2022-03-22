165d48e67d20f410911e63d687f4fcd38514869c
GuilhermeFiani <gfiani10@gmail.com>
2022-03-22 16:56:46 -0400

    Criando e referenciando botões

--- app/src/main/java/com/example/principal/MainActivity.java
+++ app/src/main/java/com/example/principal/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton imgBtn1;
+    private ImageButton imgBtn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgBtn2 = findViewById(R.layout.activity_main);
+        imgBtn2 = findViewById(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.instagram.com/vyniof/");
+                        Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(i);
+            }
+            )
+        };
+
+        imgBtn1 = (ImageButton) findViewById(R.id.imgBtn1);
+        imgBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAutobiografia();
+            }
+        });
+    }
+
+    private void openAutobiografia() {
+        Intent intent = new Intent(this, Autobiografia.class);
+        startActivity(intent);
     }
 }
